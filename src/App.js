41c1890759d5cc8e7f2d421c86b301a114e7e496
Jayant Dahiya <jayantdahiyadev@gmail.com>
2024-02-22 09:18:02 +0000

    Code/comment cleanup

--- src/App.js
+++ src/App.js
@@ -7,16 +7,6 @@ import Interact from "./Components/Interact";
 function App() {
   return (
     <>
-      {/* <div className="flex max-h-screen select-none h-fit font-WorkSans">
-        <div className="m-auto">
-          <div className="w-screen lg:w-[40vw] max-h-[70vh] h-screen">
-            <Landing />
-            <Links />
-            <Interact />
-          </div>
-        </div>
-        <Background />
-      </div> */}
       <div className="relative flex items-center w-screen h-screen">
         <div className="absolute bg-black">
           <Background />
